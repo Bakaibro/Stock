ba429e1dc2fb043260e8d285fc7b906041ad69db
Aidarbek Kurmankozhoev <aidarbek.kurmankojoev.pls@gmail.com>
2024-05-27 12:50:38 +0600

    project 5

---
+++ app/src/main/java/com/example/stock/constants/ConstantApi.java
@@ -0,0 +1,6 @@
+package com.example.stock.constants;
+
+public class ConstantApi {
+    public static final String BASE_URL = "https://breaddev.pythonanywhere.com/";
+
+}
---
+++ app/src/main/java/com/example/stock/remotedata/Api.java
@@ -0,0 +1,16 @@
+package com.example.stock.remotedata;
+
+import com.example.stock.models.Order;
+import com.example.stock.models.User;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface Api {
+    @GET("api/v1/auth/users")
+    Call<List<User>> getAllUsers();
+    @GET("api/v1/order/all")
+    Call<List<Order>> getAllOrders();
+}
